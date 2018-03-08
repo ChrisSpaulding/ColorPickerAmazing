42b4ca528249c183ab44df17bd65d9392c138aaf
David <Aguiardavidm@gmail.com>
2018-03-07 21:42:03 -0700

    Added a menu!!!!!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/c/ebookfrenzy/colorpicker/MainActivity.kt
+++ app/src/main/java/c/ebookfrenzy/colorpicker/MainActivity.kt
@@ -1,19 +1,14 @@
 package c.ebookfrenzy.colorpicker
 
-import android.content.Context
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.transition.Slide
-import android.transition.TransitionManager
 import android.util.Log
 import android.view.*
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.ArrayList
-import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.popup_layout.*
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     var red = 0
     var blue = 0
     var green = 0
-    var savedColors : ArrayList<customColor> = ArrayList()
+    var savedColors: ArrayList<customColor> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.my_toolbar))
         header_image.alpha = 1f
 
-        colorArea.setBackgroundColor(Color.argb(alpha,red,green,blue))
+        colorArea.setBackgroundColor(Color.argb(alpha, red, green, blue))
 
         val redSeekBar = findViewById<SeekBar>(R.id.redBar)
         redSeekBar.max = alpha
@@ -49,22 +44,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var id = item!!.title
+        Log.i("Title", "The title is ;$id")
+        for (color in savedColors) {
+            if(color.name == id){
+                red = color.red
+                blue = color.blue
+                green = color.green
+                setColor()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setColor(){
+        redBar.progress = red
+        redNumber.text = red.toString()
+
+        blueBar.progress = blue
+        blueNumber.text = blue.toString()
+
+        greenBar.progress = green
+        greenNumber.text = green.toString()
+
+        colorArea.setBackgroundColor(Color.argb(alpha,red,green,blue))
+    }
 
     private fun setSeekBarListeners(seekBar: SeekBar) {
         seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                when(seekBar!!.id) {
+                when (seekBar!!.id) {
                     R.id.redBar -> {
                         red = seekBar!!.progress
                         redNumber.text = red.toString()
                     }
-                    R.id.greenBar ->{
+                    R.id.greenBar -> {
                         green = seekBar!!.progress
                         greenNumber.text = green.toString()
                     }
@@ -78,116 +100,17 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun saveOnClick(view: View){
-        Log.i("saveOnClick","Save button Clicked")
-        val colorName:String = colorNameField.text.toString()
+    fun saveOnClick(view: View) {
+        Log.i("saveOnClick", "Save button Clicked")
+        val colorName: String = colorNameField.text.toString()
         if (colorName == "Name")
             Toast.makeText(view.context, "Please Select a Name", Toast.LENGTH_SHORT).show()
         else {
             Toast.makeText(view.context, "Saving The Color $red $green $blue $colorName", Toast.LENGTH_LONG).show()
             savedColors.add(customColor(red, green, blue, colorName))
-            Log.i("toStringColor",savedColors.get(savedColors.size-1).toString())
+
+            //add the color to the menu
+
         }
-    }
-
-    fun restoreOnClick(view:View){
-        Log.i("restoreOnClick", "Restore Button Clicked")
-        for (color in savedColors)
-            Log.i("listColors", "$color")
-
-        // Initialize a new layout inflater instance
-        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-        // Inflate a custom view using layout inflater
-        val view = inflater.inflate(R.layout.popup_layout,null)
-
-        // Initialize a new instance of popup window
-        val popupWindow = PopupWindow(
-                view, // Custom view to show in popup window
-                LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
-                LinearLayout.LayoutParams.WRAP_CONTENT // Window height
-        )
-
-        // Set an elevation for the popup window
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            popupWindow.elevation = 10.0F
-        }
-
-        // If API level 23 or higher then execute the code
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            // Create a new slide animation for popup window enter transition
-            val slideIn = Slide()
-            slideIn.slideEdge = Gravity.TOP
-            popupWindow.enterTransition = slideIn
-
-            // Slide animation for popup window exit transition
-            val slideOut = Slide()
-            slideOut.slideEdge = Gravity.RIGHT
-            popupWindow.exitTransition = slideOut
-        }
-
-        // Get the widgets reference from custom view
-        val buttonPopup = view.findViewById<Button>(R.id.button_popup)
-
-        var stringSavedColors = ArrayList<String>()
-        for (color in savedColors)
-            stringSavedColors.add(color.toString())
-
-
-        if(!savedColors.isEmpty()){
-            //val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringSavedColors)
-            list_view.adapter = MyCustomAdapter(this)
-        }
-
-        // Set a click listener for popup's button widget
-        buttonPopup.setOnClickListener{
-            // Dismiss the popup window
-            popupWindow.dismiss()
-        }
-
-        // Set a dismiss listener for popup window
-        popupWindow.setOnDismissListener {
-            Toast.makeText(applicationContext,"Popup closed",Toast.LENGTH_SHORT).show()
-        }
-
-        // Finally, show the popup window on app
-        TransitionManager.beginDelayedTransition(root_layout)
-        popupWindow.showAtLocation(
-                root_layout, // Location to display popup window
-                Gravity.CENTER, // Exact position of layout to display popup
-                0, // X offset
-                0 // Y offset
-        )
-    }
-}
-
-private class MyCustomAdapter(context : Context): BaseAdapter() {
-
-    private val mContext: Context
-
-    init{
-        mContext = context
-    }
-
-    //responsible for how many rows in my list
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    //
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    //
-    override fun getItem(p0: Int): Any {
-        return "Test String"
-    }
-
-    //renders out each row
-    override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
-        val textView = TextView(mContext)
-        textView.text = "HERE is my row for my LISTVIEW"
-        return textView
     }
 }
